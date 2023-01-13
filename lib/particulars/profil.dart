9e31e9976a874a535d334bca7920637034e871a7
Jean-Elie <jeaneliekubembula@gmail.com>
2023-01-13 12:12:27 +0100

    add workers and particular files their directories

---
+++ lib/particulars/profil.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Profil extends StatefulWidget {
+  const Profil({super.key});
+
+  @override
+  State<Profil> createState() => _ProfilState();
+}
+
+class _ProfilState extends State<Profil> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
