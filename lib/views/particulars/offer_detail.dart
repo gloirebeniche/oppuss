ebd99bb4349bdffd1b7cca04a83b8c50b2a88241
Jean-Elie <jeaneliekubembula@gmail.com>
2023-02-07 15:54:17 +0100

    Finision complète de l'interface détailer d'une offre

--- lib/views/particulars/offer_detail.dart
+++ lib/views/particulars/offer_detail.dart
@@ -2,6 +2,7 @@ import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:oppuss/utils/theme.dart';
 import 'package:oppuss/widget/button_widget_app.dart';
+import 'package:oppuss/widget/particular/app_widgets.dart';
 
 class OfferDetailView extends StatefulWidget {
   const OfferDetailView({super.key});
@@ -41,17 +42,20 @@ class _OfferDetailViewState extends State<OfferDetailView> {
               )
             ),
           Container(
-            padding: padding,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                customeTextStyle("Construction de boutique", 18, black, fontWeight: FontWeight.bold),
                 Padding(
-                  padding: const EdgeInsets.only(top: 10),
+                  padding: const EdgeInsets.only(left: 15, right: 15),
+                  child: customeTextStyle("Construction de boutique", 18, black, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 15, right: 15),
                   child: customeTextStyle("Mardi 28 février 2023 de 12h:00 à 14h:30 (2h:30)", 13, grey2),
                 ),
                 Container(
                   margin: const EdgeInsets.only(top: 25),
+                  padding: const EdgeInsets.only(left: 15, right: 15),
                   child: Row(
                     children: [
                       icon3(EvaIcons.pin),
@@ -64,6 +68,7 @@ class _OfferDetailViewState extends State<OfferDetailView> {
                 ),
                 Container(
                   margin: const EdgeInsets.only(top: 15),
+                  padding: const EdgeInsets.only(left: 15, right: 15),
                   child: Row(
                     children: [
                       icon3(EvaIcons.person),
@@ -85,6 +90,7 @@ class _OfferDetailViewState extends State<OfferDetailView> {
                 Container( margin: const EdgeInsets.only(top: 20), child: const Divider(height: 5, thickness: 1,),),
                 Container(
                   margin: const EdgeInsets.only(top: 20),
+                  padding: const EdgeInsets.only(left: 15, right: 15),
                   child: Row(
                     children: [
                       icon3(EvaIcons.messageCircleOutline),
@@ -100,10 +106,24 @@ class _OfferDetailViewState extends State<OfferDetailView> {
                 ),
                 Container( margin: const EdgeInsets.only(top: 10), child: const Divider(height: 5, thickness: 1,),),
                 Padding(
-                  padding: const EdgeInsets.only(top: 20),
+                  padding: const EdgeInsets.only(top: 20, left: 15),
                   child: customeTextStyle("Offres (10)", 20, black, fontWeight: FontWeight.bold),
                 ),
-
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  color: grey,
+                  child: Column(
+                    children: [
+                      cardOffer(),
+                      cardOffer(),
+                      cardOffer(),
+                      cardOffer(),
+                      cardOffer(),
+                      cardOffer(),
+                      cardOffer(),
+                    ],
+                  ),
+                )
               ],
             ),
           )
