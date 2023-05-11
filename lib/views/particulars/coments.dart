8f308956e9a93bac23869a8d73021545a971ee6a
Jean-Elie <jeaneliekubembula@gmail.com>
2023-05-11 17:48:27 +0200

    Finition de l'interface commentaire

--- lib/views/particulars/coments.dart
+++ lib/views/particulars/coments.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:oppuss/utils/theme.dart';
 import 'package:oppuss/widget/customized_appbar.dart';
 
 class ComentView extends StatefulWidget {
@@ -13,6 +14,260 @@ class _ComentViewState extends State<ComentView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar2("Questions et réponses", context),
+      backgroundColor: white,
+      body: ListView(
+        children: [
+          Container(
+            color: white,
+            width: double.infinity,
+            padding: const EdgeInsets.only(left: 10),
+            margin: const EdgeInsets.only(top: 10),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 30,// Rayon du cercle
+                  backgroundColor: grey,
+                  //backgroundImage: AssetImage('chemin_vers_l_image'), // Image à afficher
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // Non de l'ouvrier
+                            customeTextStyle("Bakary", 17, black, fontWeight: FontWeight.bold),
+                            // date
+                            customeTextStyle("il y a 4 jours", 13, grey2)
+                          ],
+                        ),
+                        // contenue
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: customeTextStyle(" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. ",
+                            15, black
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ), 
+          const Divider(thickness: 0.5, color: grey,),
+          Container(
+            color: white,
+            width: double.infinity,
+            padding: const EdgeInsets.only(left: 10),
+            margin: const EdgeInsets.only(top: 10),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 30,// Rayon du cercle
+                  backgroundColor: grey,
+                  //backgroundImage: AssetImage('chemin_vers_l_image'), // Image à afficher
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // Non de l'ouvrier
+                            customeTextStyle("Bakary", 17, black, fontWeight: FontWeight.bold),
+                            // date
+                            customeTextStyle("il y a 4 jours", 13, grey2)
+                          ],
+                        ),
+                        // contenue
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: customeTextStyle(" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. ",
+                            15, black
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const Divider(thickness: 0.5, color: grey,),
+          Container(
+            color: white,
+            width: double.infinity,
+            padding: const EdgeInsets.only(left: 10),
+            margin: const EdgeInsets.only(top: 10),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 30,// Rayon du cercle
+                  backgroundColor: grey,
+                  //backgroundImage: AssetImage('chemin_vers_l_image'), // Image à afficher
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // Non de l'ouvrier
+                            customeTextStyle("Bakary", 17, black, fontWeight: FontWeight.bold),
+                            // date
+                            customeTextStyle("il y a 4 jours", 13, grey2)
+                          ],
+                        ),
+                        // contenue
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: customeTextStyle(" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. ",
+                            15, black
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const Divider(thickness: 0.5, color: grey,),
+          Container(
+            color: white,
+            width: double.infinity,
+            padding: const EdgeInsets.only(left: 10),
+            margin: const EdgeInsets.only(top: 10),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 30,// Rayon du cercle
+                  backgroundColor: grey,
+                  //backgroundImage: AssetImage('chemin_vers_l_image'), // Image à afficher
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // Non de l'ouvrier
+                            customeTextStyle("Bakary", 17, black, fontWeight: FontWeight.bold),
+                            // date
+                            customeTextStyle("il y a 4 jours", 13, grey2)
+                          ],
+                        ),
+                        // contenue
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: customeTextStyle(" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. ",
+                            15, black
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const Divider(thickness: 0.5, color: grey,),
+          Container(
+            color: white,
+            width: double.infinity,
+            padding: const EdgeInsets.only(left: 10),
+            margin: const EdgeInsets.only(top: 10),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 30,// Rayon du cercle
+                  backgroundColor: grey,
+                  //backgroundImage: AssetImage('chemin_vers_l_image'), // Image à afficher
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // Non de l'ouvrier
+                            customeTextStyle("Bakary", 17, black, fontWeight: FontWeight.bold),
+                            // date
+                            customeTextStyle("il y a 4 jours", 13, grey2)
+                          ],
+                        ),
+                        // contenue
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: customeTextStyle(" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. ",
+                            15, black
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const Divider(thickness: 0.5, color: grey,),
+          Container(
+            color: white,
+            width: double.infinity,
+            padding: const EdgeInsets.only(left: 10),
+            margin: const EdgeInsets.only(top: 10),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 30,// Rayon du cercle
+                  backgroundColor: grey,
+                  //backgroundImage: AssetImage('chemin_vers_l_image'), // Image à afficher
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // Non de l'ouvrier
+                            customeTextStyle("Bakary", 17, black, fontWeight: FontWeight.bold),
+                            // date
+                            customeTextStyle("il y a 4 jours", 13, grey2)
+                          ],
+                        ),
+                        // contenue
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: customeTextStyle(" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. ",
+                            15, black
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
