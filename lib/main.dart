5ce4761f3b875e9c768c7c2b9fbdebef78975c45
Jean-Elie <jeaneliekubembula@gmail.com>
2023-07-06 00:05:06 +0200

    MAJ

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:oppuss/views/splash_screen.dart';
 import 'package:provider/provider.dart';
 import 'api/auth_provider.dart';
-import 'views/particulars/demande/reservation.dart';
 
 void main() async {
   Get.testMode = true;
@@ -39,7 +38,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ReservationView(),
+      home: SplashScreen(),
     );
   }
 }
